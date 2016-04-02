ec4f90ac5a110d63391875c5404a435225e45791
Reapy13 <hugo.fauchery@gmail.com>
2016-04-02 17:47:40 +0200

    Ajout Angular

---
+++ assets/js/angular/controller/LexiqueCtrl.js
@@ -0,0 +1,113 @@
+function LexiqueCtrl($scope) {
+    $scope.termes = [
+        {
+            "name" : "Alimentation",
+            "description" : "Composant alimentant les parties de l'ordinateur en électricité."
+        },
+        {
+
+            "name" : "BIOS",
+            "description" : "Micrologiciel de la MB effectuant des opérations élémentaires, pouvant s'ouvrir lors du démarrage de l'ordinateur."
+        },
+        {
+            "name" : "Bit",
+            "description" : "Unité de codage des données."
+        },
+        {
+            "name" : "Boitier",
+            "description" : "Contient les composants de l'ordinateur."
+        },
+        {
+            "name" : "Bus",
+            "description" : "transporte les données entre les composants de l'ordinateur."
+        },
+        {
+            "name" : "Chipset",
+            "description" : ""
+        },
+        {
+            "name" : "Core",
+            "description" : "Coeur d'un processus"
+        },
+        {
+            "name" : "Disque Dur",
+            "description" : "Mémoire de masse où sont stockées les données de l'ordinateur."
+        },
+        {
+            "name" : "DVI",
+            "description" : "Port de branchement audio-visuel."
+        },
+        {
+            "name" : "GPU",
+            "description" : "Carte graphique (Graphic Process Unit). Effectue la majorité des calculs d'affichage."
+        },
+        {
+            "name" : "HDMI",
+            "description" : "Le HDMI est un port de branchement audio-visuel de haute qualité, apportant une image pouvant aller à 1080p."
+        },
+        {
+            "name" : "Hertz (Hz)",
+            "description" : "C'est l'unité de mesure de la fréquence. On en parle notamment de Hz pour le CPU, exprimé en GHz. Un GHz = 1 000 000 000Hz."
+        },
+        {
+            "name" : "MB",
+            "description" : "Carte Mère (Mother Board). Acceuille tous les autres composants."
+        },
+        {
+            "name" : "Octet (o)",
+            "desccription" : "Un octet correspond à 8 bits."
+        },
+        {
+            "name" : "Operating System (OS)",
+            "description" : "Interface graphique permettant d'intéragir avec l'ordinateur."
+        },
+        {
+            "name" : "PCI",
+            "description" : "Port permettant d'installer la GPU, différentes cartes, etc..."
+        },
+        {
+            "name" : "Processeur (CPU)",
+            "description" : "Unité de contrôle de l'ordinateur. Il représente le cerveau et effectue la majorité des calculs."
+        },
+        {
+            "name" : "Random Access Memory (RAM)",
+            "description" : "Mémoire vive. Elle est utilisée pour stocker des données utilisées dans les programmes en cours. Elle a un accès plus rapide que les HDD/SDD."
+        },
+        {
+            "name" : "Sata",
+            "description" : "Port reliant la MB à des espaces de stockages."
+        },
+        {
+            "name" : "Socket",
+            "description" : "Emplacement de la MB pour le processeur."
+        },
+        {
+            "name" : "Souris",
+            "description" : "Accessoire servant à déplacer et à intéragir avec le curseur."
+        },
+        {
+            "name" : "UEFI",
+            "description" : ""
+        },
+        {
+            "name" : "USB",
+            "description" : "Système de transmission des données entre différents support de données."
+        },
+        {
+            "name" : "VGA",
+            "description" : "Port audio-visuel."
+        },
+        {
+            "name" : "Ventirad",
+            "description" : "Système de refroidissement à base de ventilateur et de radiateur."
+        },
+        {
+            "name" : "Watercooling",
+            "description" : "Système de refroidissement à base de liquide."
+        },
+        {
+            "name" : "Watt (W)",
+            "description" : "Unité de mesure de la puissance, utilisée pour l'alimentation."
+        }
+    ];
+}
