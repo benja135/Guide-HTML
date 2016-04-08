c86b4f0fb8f174f7378a46d977257c1d4f032e87
Maximilien <Maximilien>
2016-04-08 13:08:13 +0200

    Max ne sait pas gérer les conflits

---
+++ assets/js/angular/controller/LexiqueCtrl.js
@@ -0,0 +1,113 @@
+function LexiqueCtrl($scope) {
+    $scope.termes = [
+        {
+            "name" : "Alimentation",
+            "description" : "Composant alimentant les parties de l'ordinateur en électricité."
+        },
+        {
+
+            "name" : "BIOS",
+            "description" : "Micrologiciel de la carte mère effectuant des opérations élémentaires, pouvant s'ouvrir lors du démarrage de l'ordinateur."
+        },
+        {
+            "name" : "Bit",
+            "description" : "Unité de codage des données."
+        },
+        {
+            "name" : "Boîtier",
+            "description" : "Contient les composants de l'ordinateur."
+        },
+        {
+            "name" : "Bus",
+            "description" : "Transporte les données entre les composants de l'ordinateur."
+        },
+        {
+            "name" : "Carte Graphique (GPU)",
+            "description" : "Graphic Process Unit. Effectue la majorité des calculs d'affichage."
+        },
+        {
+            "name" : "Carte Mère (MB)",
+            "description" : "Mother Board. Accueille tous les autres composants."
+        },
+        {
+            "name" : "Chipset",
+            "description" : "Jeu de composants électroniques situé dans la carte mère. Permet de gérer les flux de données entre le processeur, la mémoire et les autres périphériques."
+        },
+        {
+            "name" : "Core",
+            "description" : "Coeur d'un processus"
+        },
+        {
+            "name" : "Disque Dur",
+            "description" : "Mémoire de masse où sont stockées les données de l'ordinateur."
+        },
+        {
+            "name" : "DVI",
+            "description" : "Port de branchement audio-visuel."
+        },
+        {
+            "name" : "HDMI",
+            "description" : "Port de branchement audio-visuel de haute qualité, apportant une image pouvant aller à 1080p."
+        },
+        {
+            "name" : "Hertz (Hz)",
+            "description" : "Unité de mesure de la fréquence. On en parle notamment pour le processeur, exprimé en GHz. 1 GHz = 1 000 000 000 Hz."
+        },
+        {
+            "name" : "Octet (o)",
+            "desccription" : "1 octet = 8 bits."
+        },
+        {
+            "name" : "Operating System (OS)",
+            "description" : "Système d'exploitation. Interface graphique permettant d'interagir avec l'ordinateur."
+        },
+        {
+            "name" : "PCI",
+            "description" : "Port permettant d'installer différentes cartes, etc..."
+        },
+        {
+            "name" : "Processeur (CPU)",
+            "description" : "Control Process Unit. Unité de contrôle de l'ordinateur. Il représente le cerveau et effectue la majorité des calculs."
+        },
+        {
+            "name" : "RAM",
+            "description" : "Mémoire vive (Random Access Memoory). Utilisée pour stocker des données utilisées dans les programmes en cours. Elle a un accès plus rapide que les disques durs."
+        },
+        {
+            "name" : "Sata",
+            "description" : "Port reliant la carte mère à des espaces de stockages."
+        },
+        {
+            "name" : "Socket",
+            "description" : "Emplacement de la carte mère pour le processeur."
+        },
+        {
+            "name" : "Souris",
+            "description" : "Accessoire servant à déplacer et à intéragir avec le curseur."
+        },
+        {
+            "name" : "UEFI",
+            "description" : "Unified Extensible Firmware Interface. BIOS amélioré. Permet de naviguer dans les options de configuration de la carte mère et du matériel."
+        },
+        {
+            "name" : "USB",
+            "description" : "Système de transmission des données entre différents supports de données."
+        },
+        {
+            "name" : "Ventirad",
+            "description" : "Système de refroidissement à base de ventilateur et de radiateur."
+        },
+        {
+            "name" : "VGA",
+            "description" : "Port de branchement audio-visuel."
+        },
+        {
+            "name" : "Watercooling",
+            "description" : "Système de refroidissement à base de liquide."
+        },
+        {
+            "name" : "Watt (W)",
+            "description" : "Unité de mesure de la puissance, utilisée pour l'alimentation."
+        }
+    ];
+}
